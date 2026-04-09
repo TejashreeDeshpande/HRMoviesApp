0a3d27e2d0f1eba689a23ea28108b1718ed802da
tejashree <tejashreek@gmail.com>
2026-04-08 20:28:18 -0700

    Add default movie list when API is failing

--- app/src/main/java/com/example/hrmoviesapp/domain/MovieRepository.kt
+++ app/src/main/java/com/example/hrmoviesapp/domain/MovieRepository.kt
@@ -1,10 +1,17 @@
 package com.example.hrmoviesapp.domain
 
 import com.example.hrmoviesapp.data.MovieData
-import com.example.hrmoviesapp.data.MovieResponse
+import com.example.hrmoviesapp.presentation.DefaultMovies
 
 class MovieRepository(private val api: MovieApi) {
     suspend fun fetchMovies(): List<MovieData> {
-        return api.fetchMovies().movieData
+        return try {
+            val response = api.fetchMovies().movieData
+            response.ifEmpty {
+                DefaultMovies.list
+            }
+        } catch (e: Exception) {
+            DefaultMovies.list
+        }
     }
 }
---
+++ app/src/main/java/com/example/hrmoviesapp/presentation/DefaultMovies.kt
@@ -0,0 +1,15 @@
+package com.example.hrmoviesapp.presentation
+
+import com.example.hrmoviesapp.data.MovieData
+
+
+object DefaultMovies {
+
+    val list = listOf(
+        MovieData("Batman Begins", 2005, "tt0372784"),
+        MovieData("The Dark Knight", 2008, "tt0468569"),
+        MovieData("The Dark Knight Rises", 2012, "tt1345836"),
+        MovieData("Batman v Superman", 2016, "tt2975590"),
+        MovieData("Justice League", 2017, "tt0974015")
+    )
+}
